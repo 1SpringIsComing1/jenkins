cb059c90bcb61fd568cc4d3b64ee3d1b75f03bab
Maksym_Kruhovykh <Maksym_kruhovykh@epam.com>
2022-01-17 19:42:06 +0200

    Initial commit

---
+++ src/test/java/com/kruhovy/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kruhovy.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
